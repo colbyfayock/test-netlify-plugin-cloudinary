8ae055374872208ff492daa0289338c14ad27eb0
colbyfayock <fayock@gmail.com>
2022-02-07 16:00:41 -0500

    test

--- plugins/netlify-plugin-cloudinary/src/index.js
+++ plugins/netlify-plugin-cloudinary/src/index.js
@@ -64,7 +64,7 @@ module.exports = {
 
     netlifyConfig.redirects.push({
       from: path.join('/images/', ':image'),
-      to: `${process.env.DEPLOY_PRIME_URL}/${path.join('.netlify', 'functions', name)}`,
+      to: `${process.env.DEPLOY_PRIME_URL}/${path.join('.netlify', 'functions', name, ':image')}`,
       status: 301,
       force: true
     });
