b8cbd13658d3c828a35686822ed1418e73380e42
colbyfayock <fayock@gmail.com>
2022-02-07 16:13:15 -0500

    est

--- plugins/netlify-plugin-cloudinary/src/templates/images.js
+++ plugins/netlify-plugin-cloudinary/src/templates/images.js
@@ -1,13 +1,14 @@
-const functionName = 'cld_images';
-
 exports.handler = async function(event, context) {
 
-  const { path } = event;
-  const imagePath = `/images${path.split(functionName)[1]}`;
+  const { rawUrl } = event;
+  const pathSegments = rawUrl.split('.netlify/functions/cld_images');
+  const baseUrl = pathSegments[0];
+  const imagePath = `/images${pathSegments[1]}`;
 
   return {
     statusCode: 200,
     body: JSON.stringify({
+      baseUrl,
       imagePath,
       event,
       context
