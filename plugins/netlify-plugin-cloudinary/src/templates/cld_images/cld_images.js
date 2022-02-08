51f976c3ffb9e7135c403b5152610a4403d160eb
colbyfayock <fayock@gmail.com>
2022-02-08 14:34:56 -0500

    test

--- plugins/netlify-plugin-cloudinary/src/templates/cld_images/cld_images.js
+++ plugins/netlify-plugin-cloudinary/src/templates/cld_images/cld_images.js
@@ -1,67 +1,37 @@
 exports.handler = async function (event, context) {
-  console.log('event', event);
-  console.log('context', context);
-
-  const { rawUrl, headers } = event;
+  const { rawUrl } = event;
 
   const rawUrlSegments = rawUrl.split('.netlify/functions/cld_images');
   const endpoint = rawUrlSegments[0].replace(/\/$/, '');
   const pathSegments = rawUrlSegments[1].split('?');
-  const imagePath = `/cld-assets/images/${pathSegments[0]}`;
+  const imagePath = `/cld-assets/images${pathSegments[0]}`;
 
   const { deliveryType, uploadPreset } = getQueryParams(rawUrl);
 
   const cloudName = process.env.CLOUDINARY_CLOUD_NAME || queryParams.cloudName;
 
-  console.log('rawUrl', rawUrl)
-  console.log('pathSegments', pathSegments)
-  console.log('imagePath', imagePath)
-  console.log('cloudName', cloudName)
-  console.log('endpoint', endpoint)
-  console.log('imagePath', imagePath)
-
   const remoteUrl = encodeURIComponent(`${endpoint}${imagePath}`);
-  console.log('remoteUrl', remoteUrl)
+
   const cloudinaryUrl = `https://res.cloudinary.com/colbydemo/image/fetch/f_auto,q_auto/${remoteUrl}`
 
-  console.log('cloudinaryUrl', cloudinaryUrl)
+  console.log({
+    rawUrl,
+    pathSegments,
+    imagePath,
+    cloudName,
+    endpoint,
+    imagePath,
+    remoteUrl,
+    cloudinaryUrl
+  })
 
   return {
     statusCode: 302,
     headers: {
-      Location: cloudinaryUrl,
-      // 'Set-Cookie': 'colbycld=true'
+      Location: cloudinaryUrl
     }
   };
 };
-
-/**
- * isRemoteUrl
- */
-
-function isRemoteUrl(url) {
-  return url.startsWith('http');
-}
-
-module.exports.isRemoteUrl = isRemoteUrl;
-
-/**
- * determineRemoteUrl
- */
-
-function determineRemoteUrl(url, host) {
-  if ( isRemoteUrl(url) ) return url;
-
-  if ( !url.startsWith('/') ) {
-    url = `/${url}`;
-  }
-
-  url = `${host}${url}`;
-
-  return url;
-}
-
-module.exports.determineRemoteUrl = determineRemoteUrl;
 
 /**
  * getQueryParams
